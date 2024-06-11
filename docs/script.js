0bc4ca2fb8d2ff0de904e49b70683a527af66486
Denis Ryzhkov <denisr@denisr.com>
2024-06-11 17:04:18 +0200

    WIP

--- docs/script.js
+++ docs/script.js
@@ -2,11 +2,14 @@
   const elem = (id) => document.getElementById(id);
 
   const main = () => {
+    // TextArea:
     const ta = elem("ta");
+
+    // "Loading..." is finished:
     ta.value = "";
     ta.readOnly = false;
     ta.focus();
-  }
+  };
   
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", main);
