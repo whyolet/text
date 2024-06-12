055b6020e17399aeedbb11cd2f761d41f17413cf
Denis Ryzhkov <denisr@denisr.com>
2024-06-12 08:22:19 +0200

    WIP

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,23 @@
-(function() {
+(() => {
+
+  window.addEventListener("error", (e) => {
+    window.location.assign(
+      "mailto:support@whyolet.com" +
+      "?subject=Whyolet%20Text" +
+      "&body=" +
+      encodeURIComponent([
+        "Error! Please help to fix it by writing a line how it happened and/or sending this info:",
+        "",
+        e.message || "",
+        (e.filename || "") + ":" +
+        (e.lineno || "") + ":" +
+        (e.colno || ""),
+        "",
+        e.error && e.error.stack || "",
+      ].join("\n"))
+    ); 
+  });
+
   const elem = (id) => document.getElementById(id);
 
   const main = () => {
