e4a90522b27a1410fe6a9ef07d17d6afd84c3200
Denis Ryzhkov <denisr@denisr.com>
2024-12-21 20:52:40 +0100

    Indent

--- docs/v2/indent.js
+++ docs/v2/indent.js
@@ -1,8 +1,10 @@
 import * as db from "./db.js";
 import {getSel} from "./sel.js";
+import {save} from "./page.js";
 import {ui} from "./ui.js";
 
 const mem = db.mem;
+const indent = "  ";
 
 /// autoindent
 
@@ -38,20 +40,37 @@ export const autoindent = () => {
   .slice(0, i)
   .match(/([^\r\n]*)([\r\n]*)$/);
 
-  const indent = templateLine.match(/^[\t ]*/)[0];
+  const indents = templateLine.match(/^[\t ]*/)[0];
 
-  if (templateLine === indent) {
-    // Move indent from template line to current line, to keep blank lines clean.
+  if (templateLine === indents) {
+    // Move indents from template line to current line, to keep blank lines clean.
     ui.ta.setRangeText(
-      emptyLines + indent,
+      emptyLines + indents,
       i - matchedLines.length,
       i,
       "end",
      );
   } else {
-    // Copy indent from template line to current line.
-    ui.ta.setRangeText(indent, i, i, "end");
+    // Copy indents from template line to current line.
+    ui.ta.setRangeText(indents, i, i, "end");
   }
 
   mem.textLength = ui.ta.value.length;
+};
+
+/// onIndent
+
+export const onIndent = async () => {
+  const {start, end, part} = getSel({wholeLines: true});
+
+  const result = part
+  .split("\n")
+  .map(line => /\S/.test(line) ?
+    indent + line
+    : line
+  )
+  .join("\n");
+  
+  ui.ta.setRangeText(result, start, end, "select");
+  await save();
 };
