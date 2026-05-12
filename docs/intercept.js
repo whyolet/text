aa83f81f08e94996c94dd052411929090ac61986
Denis Ryzhkov <denisr@denisr.com>
2026-05-12 09:28:15 +0200

    Detect sync broken by "Open in browser"

--- docs/intercept.js
+++ docs/intercept.js
@@ -1,11 +1,15 @@
-if (
-  window.opener &&
-  location.hash.includes("state")
-) {
-  window.opener.postMessage({
-    "type": "oauth-redirect",
-    "hash": location.hash,
-  }, location.origin);
+if (location.hash.includes("state")) {
+  if (window.opener) {
+    window.opener.postMessage({
+      "type": "oauth-redirect",
+      "hash": location.hash,
+    });
+  } else {
+    location.hash = "";
+    alert(`Please don't
+"Open in browser":
+it breaks the sync!`);
+  }
 
   window.close();
 
